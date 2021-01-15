e0c6a226df5df33f758567ff6cf63754117681cd
lenovo <lenovo@DESKTOP-HOO5BVA>
2021-01-15 17:48:24 +0530

    lab_05

---
+++ Lab_05/src/InvalidAgeException.java
@@ -0,0 +1,12 @@
+
+public class InvalidAgeException extends Exception {
+	public InvalidAgeException(String s)
+
+    {
+		super(s);
+		System.out.println("Age is below 15");
+        // Call constructor of parent Exception
+
+    }
+
+}
---
+++ Lab_05/src/NameException.java
@@ -0,0 +1,11 @@
+
+public class NameException extends Exception {
+
+	public NameException(String string) {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+
+
+}
